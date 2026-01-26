98ddbc89791c91ddcf1ad5ecccd8a615c57b19cd
GustavoCasiano <gustavocasiano@escire.lat>
2026-01-26 14:17:24 -0600

    Init: Version estable

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @defgroup plugins_generic_mp3ArticleGalley HTML Article Galley Plugin
+ */
+
+/**
+ * @file plugins/generic/mp3ArticleGalley/index.php
+ *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_generic_mp3ArticleGalley
+ * @brief Wrapper for html article galley plugin.
+ *
+ */
+
+require_once('Mp3ArticleGalleyPlugin.inc.php');
+
+return new Mp3ArticleGalleyPlugin();
+
+
